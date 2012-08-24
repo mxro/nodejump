88c02dedfbce9bd1c3fc6f925614452f4925e086
mroh004 <mroh004@dev>
2012-08-24 14:53:54 +1200

    building app module

--- 0.0.1/nodejump-app-0.0.1.js
+++ 0.0.1/nodejump-app-0.0.1.js
@@ -1,1 +1,22 @@
-// <!-- one.createPublic 
+// <!-- one.upload https://u1.linnk.it/qc8sbw/usr/apps/ajMicroSync/docs/nodejump-app-0.0.1 -->
+
+(function($, AJ) {
+
+	$.initNodeJumpApp = function(params) {
+		
+		var nj = {};
+		var elem = params.elem;
+		
+		nj.initUi() = function() {
+			
+		};
+		
+		
+		return {
+			initUi : nj.initUi
+		};
+	};
+	
+})(jQuery, AJ);
+
+// <!-- one.end -->
